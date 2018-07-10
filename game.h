ec84d1f5d086a10bda6dad08cf439be0fc504d97
adityah-b <bhattacharjee.aditya@gmail.com>
2018-07-09 20:07:16 -0400

    Game Working

--- game.h
+++ game.h
@@ -6,8 +6,8 @@
 #include "game_bitmaps.h"
 
 // Macros
-#define MAX_SCREEN_LENGTH 20
-#define MAX_SCREEN_WIDTH 20
+#define MAX_SCREEN_LENGTH 50
+#define MAX_SCREEN_WIDTH 50
 #define GOLD 10
 #define SILVER 5
 #define COPPER 1
@@ -20,25 +20,33 @@
 #define SURFACE 2
 #define NUM_LEDS 8
 #define FUEL_TIME 10
+#define END_GAME_THRESHOLD 70
+#define NUM_SCROLL 8
 
 // Global Variables
 uint32_t min_row = 0, min_col = 0;
 uint32_t max_row = 16, max_col = 12;
 uint32_t fuel_x, fuel_y;
 uint32_t ms_ticks;
-bool gameOver = 0;
+uint32_t num_slides = 0;
+bool game_over = 0;
 char map[MAX_SCREEN_WIDTH][MAX_SCREEN_LENGTH]; // Temporary
+bool map_scrolled = false;
+OS_SEM display_refreshed, action_performed;
+OS_MUT fuel_lock;
 
 /* Character Struct */
 typedef struct {
+	uint32_t x_pos_prev, y_pos_prev;
 	uint32_t x_pos, y_pos;
 
-	uint32_t num_gold, num_silver, num_copper;
+	uint32_t num_gold, num_emerald, num_copper;
 	uint32_t fuel_status;
 	uint32_t num_points;
 
 	bool is_flying;
 	bool select_action;
+	bool game_won;
 
 	// Robot Inputs
 	volatile uint32_t dir;
@@ -59,6 +67,8 @@ void pollPushbutton(void);
 void setLED(uint32_t val);
 void loadBMP(uint32_t row, uint32_t col);
 void initMap(void);
+void endGameDisplay(void);
+void printMap(void);
 /* Functions */
 
 /* Tasks */
